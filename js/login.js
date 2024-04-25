4d61089c67ab8039f29de947629e26ff0a3d5500
Md. Fakhrul Alam Shuvo <ifakhrul23@gmail.com>
2024-04-25 22:57:03 +0600

    added page

---
+++ js/login.js
@@ -0,0 +1,13 @@
+document.getElementById('btn-sbmt').addEventListener('click', function(){
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    const passField = document.getElementById('user-pass');
+    const password = passField.value;
+    
+    if(email === 'boss@mail.com' && password === 'pass'){
+        window.location.href = 'bank.html';
+    }
+    else{
+        alert("get the hell outta here");
+    }
+})
