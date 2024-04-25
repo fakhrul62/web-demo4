baca26f08fc899770f970f1a69a61e7211676745
Md. Fakhrul Alam Shuvo <ifakhrul23@gmail.com>
2024-04-26 01:48:13 +0600

    final changes

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,24 +3,30 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const newWithdrawString = withdrawValue.value;
     const newWithdraw = parseFloat(newWithdrawString);
 
-    // adding it to the previous deposit value
+
     const withdrawNow = document.getElementById('withdraw-total');
     const prevWithdrawTotalString = withdrawNow.innerText;
     const prevWithdrawTotal = parseFloat(prevWithdrawTotalString);
-    const currentWithdrawTotal = prevWithdrawTotal+newWithdraw;
-    withdrawNow.innerText = currentWithdrawTotal;
-
-    // getting the balance value and coverting it to string
+    
+  
     const balanceValue = document.getElementById('balance-total');
     const prevBalanceTotalString= balanceValue.innerText;
     const prevBalanceTotal = parseFloat(prevBalanceTotalString);
+ 
 
-    // adding the current deposit to the current balance
     const balanceNow =  prevBalanceTotal - newWithdraw;
-    balanceValue.innerText = balanceNow;
-
+    
 
     withdrawValue.value = '';
+    
+    if(newWithdraw>balanceNow){
+        alert("No money No honey");
+        return;
+    }
+    const currentWithdrawTotal = prevWithdrawTotal+newWithdraw;
+    withdrawNow.innerText = currentWithdrawTotal;
 
-
+  
+    balanceValue.innerText = balanceNow;
+    
 })
